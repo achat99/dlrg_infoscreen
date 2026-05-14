3712c86cd5c6b0ec16a4d7d263f3ba7c4a0b8cc2
Finn-Niklas Rathjen <f.rathjen@icloud.com>
2026-05-15 01:05:08 +0200

    change file upload size

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 10 * 1024 * 1024 },
+  limits: { fileSize: 100 * 1024 * 1024 },
   fileFilter: (_req, file, cb) => {
     const allowedMimeTypes = new Set([
       'image/jpeg',
